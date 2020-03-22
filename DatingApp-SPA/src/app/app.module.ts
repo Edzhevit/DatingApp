438efcdf79d6d5c8867e699ddaed572ea1e40dc9
Edzhevit <nightwalker_@abv.bg>
2020-03-22 18:09:00 +0200

    Add navbar to SPA application

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,22 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import {AppComponent} from './app.component';
+import {ValueComponent} from './value/value.component';
 import {HttpClientModule} from '@angular/common/http';
+import {NavComponent} from './nav/nav.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent
-   ],
-   imports: [
-      BrowserModule,
-     HttpClientModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
