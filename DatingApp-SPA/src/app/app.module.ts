503c16e356ec9353d4444f8ca8f3c7aa01facf59
Edzhevit <nightwalker_@abv.bg>
2020-03-23 12:59:31 +0200

    Add register component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,24 +7,28 @@ import {HttpClientModule} from '@angular/common/http';
 import {NavComponent} from './nav/nav.component';
 import {FormsModule} from "@angular/forms";
 import {AuthService} from "./services/auth.service";
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ValueComponent,
-    NavComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [
-    AuthService
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+   declarations: [
+      AppComponent,
+      ValueComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [
+      AuthService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {
 }
