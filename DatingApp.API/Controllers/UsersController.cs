69ba4a02c39bb0296ce3f8511f30e97949cc6bab
Edzhevit <nightwalker_@abv.bg>
2020-03-25 17:05:22 +0200

    Add user controller

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    // [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IDatingRepository _datingRepository;
+
+        public UsersController(IDatingRepository datingRepository)
+        {
+            _datingRepository = datingRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _datingRepository.GetUsers();
+            return Ok(users);
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _datingRepository.GetUser(id);
+            return Ok(user);
+        }
+    }
+}
