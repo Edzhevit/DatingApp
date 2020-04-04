4e90e12960df1c5860936eceb25ecbed4582852c
Edzhevit <nightwalker_@abv.bg>
2020-04-04 17:51:13 +0300

    Add JwT Module and configure it

--- DatingApp-SPA/src/app/services/user.service.ts
+++ DatingApp-SPA/src/app/services/user.service.ts
@@ -1,14 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {User} from '../models/user';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Authorization': 'Bearer ' + localStorage.getItem('token')
-  })
-};
 
 @Injectable({
   providedIn: 'root'
@@ -20,11 +14,11 @@ export class UserService {
   }
 
   getUsers(): Observable<User[]>{
-    return this.http.get<User[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<User[]>(this.baseUrl + 'users');
   }
 
   getUser(id): Observable<User>{
-    return this.http.get<User>(this.baseUrl + 'users/' + id, httpOptions);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
 }
