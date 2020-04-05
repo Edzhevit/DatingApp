ed3cc2d5f778d6751bb3fe29e1f9d9678403ff27
Edzhevit <nightwalker_@abv.bg>
2020-04-05 17:10:45 +0300

    Use resolvers in routes

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -3,13 +3,15 @@ import {HomeComponent} from './home/home.component';
 import {MemberListComponent} from './members/member-list/member-list.component';
 import {MessagesComponent} from './messages/messages.component';
 import {ListsComponent} from './lists/lists.component';
-import {AuthGuard} from "./guards/auth.guard";
-import {MemberDetailComponent} from "./members/member-detail/member-detail.component";
+import {AuthGuard} from './guards/auth.guard';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberDetailResolver} from './resolvers/member-detail-resolver';
+import {MemberListResolver} from './resolvers/member-list-resolver';
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-  {path: 'members/:id', component: MemberDetailComponent, canActivate: [AuthGuard]},
+  {path: 'members', resolve: {users: MemberListResolver}, component: MemberListComponent, canActivate: [AuthGuard]},
+  {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}, canActivate: [AuthGuard]},
   {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
   {path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: '', pathMatch: 'full'}
