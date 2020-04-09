f5a9d64a169bc5d248c02a5f2f518195b66f02e3
Edzhevit <nightwalker_@abv.bg>
2020-04-09 18:00:39 +0300

    Implement updateUser method

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {User} from '../../models/user';
 import {ActivatedRoute} from '@angular/router';
+import {AlertifyService} from '../../services/alertify.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,13 +10,19 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm', {static: true}) editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertifyService: AlertifyService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    this.alertifyService.success('Profile updated successfully')
+    this.editForm.reset(this.user);
   }
 }
