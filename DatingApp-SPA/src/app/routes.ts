70ec912e308903d53168024e4d574a4e34da35c3
Edzhevit <nightwalker_@abv.bg>
2020-04-09 16:56:23 +0300

    Update edit route in navbar

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,11 +7,14 @@ import {AuthGuard} from './guards/auth.guard';
 import {MemberDetailComponent} from './members/member-detail/member-detail.component';
 import {MemberDetailResolver} from './resolvers/member-detail-resolver';
 import {MemberListResolver} from './resolvers/member-list-resolver';
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import {MemberEditResolver} from "./resolvers/member-edit-resolver";
 
 export const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'members', resolve: {users: MemberListResolver}, component: MemberListComponent, canActivate: [AuthGuard]},
   {path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}, canActivate: [AuthGuard]},
+  {path: 'members/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver}, canActivate: [AuthGuard]},
   {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
   {path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: '', pathMatch: 'full'}
