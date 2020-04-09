00943549191fd5a81b9acf0107148244883a8fdc
Edzhevit <nightwalker_@abv.bg>
2020-04-09 16:57:07 +0300

    Import member edit component and resolver

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,30 +1,32 @@
-// import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {BsDropdownModule, TabsModule} from 'ngx-bootstrap';
 import {RouterModule} from '@angular/router';
 import {JwtModule} from '@auth0/angular-jwt';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {NgxGalleryModule} from 'ngx-gallery-9';
 
 import {AppComponent} from './app.component';
-import {HttpClientModule} from '@angular/common/http';
 import {NavComponent} from './nav/nav.component';
-import {FormsModule} from '@angular/forms';
-import {AuthService} from './services/auth.service';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
-import {ErrorInterceptorProvider} from './services/error.interceptor';
-import {AlertifyService} from './services/alertify.service';
 import {MemberListComponent} from './members/member-list/member-list.component';
 import {ListsComponent} from './lists/lists.component';
 import {MessagesComponent} from './messages/messages.component';
-import {appRoutes} from './routes';
 import {MemberCardComponent} from './members/member-card/member-card.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import {AuthGuard} from './guards/auth.guard';
+import {MemberDetailComponent } from './members/member-detail/member-detail.component';
+import {MemberEditComponent } from './members/member-edit/member-edit.component';
+
+import {AuthService} from './services/auth.service';
+import {AlertifyService} from './services/alertify.service';
 import {UserService} from './services/user.service';
+import {ErrorInterceptorProvider} from './services/error.interceptor';
+import {appRoutes} from './routes';
+import {AuthGuard} from './guards/auth.guard';
 import {MemberDetailResolver} from './resolvers/member-detail-resolver';
 import {MemberListResolver} from './resolvers/member-list-resolver';
-import {NgxGalleryModule} from 'ngx-gallery-9';
+import {MemberEditResolver} from './resolvers/member-edit-resolver';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -40,7 +42,8 @@ export function tokenGetter() {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -66,6 +69,7 @@ export function tokenGetter() {
     UserService,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver
   ],
   bootstrap: [
     AppComponent
