f8415710b8e32b675e98a9ade9e7e53cfb7e5a93
Edzhevit <nightwalker_@abv.bg>
2020-04-09 16:56:05 +0300

    Add member edit resolver

---
+++ DatingApp-SPA/src/app/resolvers/member-edit-resolver.ts
@@ -0,0 +1,26 @@
+﻿import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, Router} from '@angular/router';
+import {User} from '../models/user';
+import {UserService} from '../services/user.service';
+import {Observable, of} from 'rxjs';
+import {AlertifyService} from '../services/alertify.service';
+import {catchError} from 'rxjs/operators';
+import {AuthService} from "../services/auth.service";
+
+@Injectable()
+export class MemberEditResolver  implements Resolve<User>{
+
+  constructor(private userService: UserService, private authService: AuthService,
+              private router: Router, private alertifyService: AlertifyService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<User> {
+    return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+      catchError(error => {
+        this.alertifyService.error('Problem retrieving data');
+        this.router.navigate(['/members']);
+        return of(null);
+      })
+    )
+  }
+}
