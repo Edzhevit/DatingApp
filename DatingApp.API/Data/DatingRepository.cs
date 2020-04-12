07c2b5aabf4777ffcf6ea2b8d4e1cfb3db711fa9
Edzhevit <nightwalker_@abv.bg>
2020-04-12 17:39:58 +0300

    Add get photo method in dating repository

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
@@ -41,6 +42,12 @@ namespace DatingApp.API.Data
             var user = await _context.Users.Include(p => p.Photos)
                 .FirstOrDefaultAsync(u => u.Id == id);
             return user;
+        }
+
+        public async Task<Photo> GetPhoto(int id)
+        {
+            var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
+            return photo;
         }
     }
 }
