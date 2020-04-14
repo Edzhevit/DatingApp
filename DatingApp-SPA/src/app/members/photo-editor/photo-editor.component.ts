52c7e439bb0be7a0b18f0e41a28937b73fe1a98f
Edzhevit <nightwalker_@abv.bg>
2020-04-14 15:49:07 +0300

    Initialize the uploader in photo editor component and confiruge its options

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {Photo} from "../../models/photo";
+import {Photo} from '../../models/photo';
+import {FileUploader} from 'ng2-file-upload';
+import {environment} from '../../../environments/environment';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-photo-editor',
@@ -9,10 +12,31 @@ import {Photo} from "../../models/photo";
 export class PhotoEditorComponent implements OnInit {
 
   @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    })
   }
 
 }
