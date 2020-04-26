427b235668be2280d81450ad91546b497bf5f03d
Edzhevit <nightwalker_@abv.bg>
2020-04-26 13:53:26 +0300

    Add ordering functionality to SPA

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import {TabsModule} from 'ngx-bootstrap';
+import {ButtonsModule, TabsModule} from 'ngx-bootstrap';
 import {RouterModule} from '@angular/router';
 import {JwtModule} from '@auth0/angular-jwt';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -56,6 +56,7 @@ export function tokenGetter() {
     FormsModule,
     ReactiveFormsModule,
     TabsModule.forRoot(),
+    ButtonsModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
     FileUploadModule,
