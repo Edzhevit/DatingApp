6ee7f3a7e275a961a05d9aaca429dee27f7c4257
Edzhevit <nightwalker_@abv.bg>
2020-04-26 16:04:17 +0300

    Implement like button in SPA

--- DatingApp-SPA/src/app/members/member-card/member-card.component.ts
+++ DatingApp-SPA/src/app/members/member-card/member-card.component.ts
@@ -1,5 +1,8 @@
 ﻿import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../models/user";
+import {AuthService} from "../../services/auth.service";
+import {UserService} from "../../services/user.service";
+import {AlertifyService} from "../../services/alertify.service";
 
 @Component({
   selector: 'app-member-card',
@@ -10,10 +13,19 @@ export class MemberCardComponent implements OnInit {
 
   @Input() user: User;
 
-  constructor() {
+  constructor(private authService: AuthService, private userService: UserService,
+              private alertifyService: AlertifyService) {
   }
 
   ngOnInit() {
+  }
+
+  sendLike(id: number){
+    this.userService.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
+      this.alertifyService.success('You have liked: ' + this.user.knownAs);
+    }, error => {
+      this.alertifyService.error(error);
+    });
   }
 
 }
