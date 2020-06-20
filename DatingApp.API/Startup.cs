6be9ae1ebb3fbdd0b6a538cd6ab9aade2915e1a4
Edzhevit <nightwalker_@abv.bg>
2020-06-20 23:13:34 +0300

    Add policies for different roles

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -77,7 +77,14 @@ namespace DatingApp.API
                         ValidateAudience = false
                     };
                 });
-            
+
+            services.AddAuthorization(opt =>
+            {
+                opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+                opt.AddPolicy("VipOnly", policy => policy.RequireRole("VIP"));
+            });
+                
             services.AddControllers(opt =>
             {
                 var policy = new AuthorizationPolicyBuilder()
