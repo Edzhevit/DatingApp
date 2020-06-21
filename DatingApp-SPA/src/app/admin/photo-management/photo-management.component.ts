a1d933c04c423496c356d092c07750a5db8ca299
Edzhevit <nightwalker_@abv.bg>
2020-06-21 21:15:30 +0300

    Implement photo management in SPA

--- DatingApp-SPA/src/app/admin/photo-management/photo-management.component.ts
+++ DatingApp-SPA/src/app/admin/photo-management/photo-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AdminService} from "../../services/admin.service";
+import {AlertifyService} from "../../services/alertify.service";
 
 @Component({
   selector: 'app-photo-management',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoManagementComponent implements OnInit {
 
-  constructor() { }
+  photos: any;
+
+  constructor(private adminService: AdminService, private alertifyService: AlertifyService) { }
 
   ngOnInit(): void {
+    this.getPhotosForApproval();
+  }
+
+  getPhotosForApproval() {
+    this.adminService.getPhotosForApproval().subscribe((photos) => {
+      this.photos = photos;
+    }, error => {
+      this.alertifyService.error(error);
+    });
+  }
+
+  approvePhoto(photoId) {
+    this.adminService.approvePhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      this.alertifyService.error(error);
+    });
+  }
+
+  reject(photoId) {
+    this.adminService.rejectPhoto(photoId).subscribe(() => {
+      this.photos.splice(this.photos.findIndex(p => p.id === photoId), 1);
+    }, error => {
+      this.alertifyService.error(error);
+    });
   }
 
 }
--- DatingApp-SPA/src/app/models/photo.ts
+++ DatingApp-SPA/src/app/models/photo.ts
@@ -4,4 +4,5 @@
   description: string;
   dateAdded: Date;
   isMain: boolean;
+  isApproved: boolean;
 }
